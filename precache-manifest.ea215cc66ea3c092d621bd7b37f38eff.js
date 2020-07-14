0057c62c6db21f6865d5218219947465e4635392
Wojtek Szczawinski <szczawinski.wojtek@gmail.com>
2020-07-14 21:52:20 +0200

    Updates

---
+++ precache-manifest.ea215cc66ea3c092d621bd7b37f38eff.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bfb2669dcf06400ba5b0195c76603555",
+    "url": "/PictureSearch/index.html"
+  },
+  {
+    "revision": "13ca7300ee68d3a42723",
+    "url": "/PictureSearch/static/css/main.cdfd2af4.chunk.css"
+  },
+  {
+    "revision": "106487cba8475587305a",
+    "url": "/PictureSearch/static/js/2.d770aa87.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/PictureSearch/static/js/2.d770aa87.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "13ca7300ee68d3a42723",
+    "url": "/PictureSearch/static/js/main.b3b548b5.chunk.js"
+  },
+  {
+    "revision": "f49aa624f15325c439e2",
+    "url": "/PictureSearch/static/js/runtime-main.7fe4a6d5.js"
+  },
+  {
+    "revision": "1fa34a3968d3d0c156f04dbe0f3ded66",
+    "url": "/PictureSearch/static/media/error.1fa34a39.png"
+  },
+  {
+    "revision": "697c7c0e9740d513f4fc6194f99c14c2",
+    "url": "/PictureSearch/static/media/gh-social.697c7c0e.png"
+  }
+]);
